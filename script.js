bf2d1047b7529bfa76148f3e19b5d818f3188394
Devon Jones <devon.shun.jones@gmail.com>
2025-11-08 16:01:53 -0600

    fixed id repeats

--- script.js
+++ script.js
@@ -1,149 +1,169 @@
+// posts = [];
+const titleInput = document.getElementById("title");
+const titleInputError = document.getElementById("titleInputError");
+const titleModal = document.getElementById("titleModal");
+const bodyInput = document.getElementById("bodyText");
+const bodyInputError = document.getElementById("bodyInputError");
+const bodyModal = document.getElementById("bodyTextModal");
+const submitBtn = document.getElementById("submitButton");
+const blogForm = document.getElementById("blog-form");
+const modalForm = document.getElementById("modal-blog-form");
+const template = document.getElementById("template");
+const postDisplay = document.getElementById("blog-posts-display");
+const modalUpdate = document.getElementById("modalForm");
+const updateBtn = document.getElementById("updateButton");
+const formHalf = document.getElementById("form-half");
+const clearBtn = document.getElementById("clearBtn");
 
-
-posts=[]
-const titleInput = document.getElementById("title")
-const titleInputError = document.getElementById("titleInputError")
-const titleModal = document.getElementById("titleModal")
-const bodyInput = document.getElementById("bodyText")
-const bodyInputError = document.getElementById("bodyInputError")
-const bodyModal = document.getElementById("bodyTextModal")
-const submitBtn = document.getElementById("submitButton")
-const blogForm = document.getElementById("blog-form")
-const modalForm = document.getElementById("modal-blog-form")
-const template = document.getElementById("template")
-const postDisplay = document.getElementById("blog-posts-display")
-const modalUpdate = document.getElementById("modalForm")
-const updateBtn = document.getElementById("updateButton")
-const formHalf = document.getElementById("form-half")
-const clearBtn = document.getElementById("clearBtn")
-
-
-submitBtn.addEventListener("click",function(event){
-    if(!titleInput.validity.valid){
-        titleInputError.textContent = "Please enter a title"
-        titleInput.focus()
-    } else{
-        titleInputError.textContent = ''
-    }
-    if(!bodyInput.validity.valid){
-        bodyInputError.textContent = "Please enter a message"
-        bodyInput.focus()
-    } else{
-        bodyInputError.textContent = ''
-    }
-    if (blogForm.checkValidity()){
-        window.location.reload()
-        alert("Form Submitted Successfully")
-        createBlogPost()
-    }
-
-})
-
-
-const createBlogPost =()=>{
-    let recall;
-    try {
-        recall = JSON.parse(localStorage.getItem('savedPosts'))
-    } catch (error) {
-        console.error('Error parsing items from localStorage', e)
-        recall = null
-    }
-    if(recall == null){
-        recall = []
-    }
-    const postObject = {
-        title: titleInput.value,
-        body: bodyInput.value,
-        id: recall.length
-    }
-    recall.push(postObject)
-    localStorage.setItem('savedPosts', JSON.stringify(recall))
-    const newBlogPost = document.createElement("div")
-    newBlogPost.setAttribute("id", recall.length-1)
-    const blogPostIndex = recall.findIndex(postObject => postObject.id == newBlogPost.id)
-    newBlogPost.innerHTML = `${recall[blogPostIndex].title}:<br>${recall[blogPostIndex].body}<br><button class="remove-from-list">Delete</button> <button class="edit">Edit</button>`
-    postDisplay.appendChild(newBlogPost)
-    bodyInput.value = ''
-    titleInput.value = ''
+let objectCounter = localStorage.getItem('ObjectCounter');
+if (objectCounter === null) {
+    objectCounter = 0;
+} else {
+     objectCounter = parseInt(objectCounter, 10); 
 }
 
-    let recall;
-    try {
-        recall = JSON.parse(localStorage.getItem('savedPosts'))
-    } catch (error) {
-        console.error('Error parsing items from localStorage', e)
-        recall = null;
-    }
-const updateBlogPost =()=>{
-
-    const postObject = {
-        title: titleModal.value,
-        body: bodyModal.value,
-        id: recall.length
-    }
-    recall.push(postObject)
-    const newBlogPost = document.createElement("div")
-    newBlogPost.setAttribute("id", recall.length-1)
-    const blogPostIndex = recall.findIndex(postObject => postObject.id == newBlogPost.id)
-    newBlogPost.innerHTML = `${recall[blogPostIndex].title}:<br>${recall[blogPostIndex].body}<br><button class="remove-from-list">Delete</button> <button class="edit">Edit</button>`
-    postDisplay.appendChild(newBlogPost)
-    localStorage.setItem('savedPosts', JSON.stringify(recall))
-}
-
-postDisplay.addEventListener('click', (event) => {
-
-if (event.target.classList.contains('remove-from-list')) {
-    const postId = event.target.closest('div').id;
-    const blogPost = document.getElementById(`${postId}`)
-    const blogPostIndex = recall.findIndex(postObject => postObject.id == postId)
-    postDisplay.removeChild(blogPost);
-    recall.splice((blogPostIndex), 1)
-    localStorage.setItem('savedPosts', JSON.stringify(recall))
-} else if(event.target.classList.contains("edit")){
-    const postId = event.target.closest('div').id;
-    const blogPost = document.getElementById(`${postId}`)
-    const blogPostIndex = recall.findIndex(postObject => postObject.id == postId)
-    modalUpdate.style.display = "block";
-    formHalf.style.filter = 'blur(5px)'
-    postDisplay.style.filter = 'blur(5px)'
-    titleModal.value = recall[blogPostIndex].title
-    bodyModal.value = recall[blogPostIndex].body
-    postDisplay.removeChild(blogPost);
-    recall.splice(blogPostIndex, 1)
-    localStorage.setItem('savedPosts', JSON.stringify(recall))
-    alert(buttonPressed)
-}
+submitBtn.addEventListener("click", event => {
+  if (!titleInput.validity.valid) {
+    titleInputError.textContent = "Please enter a title";
+    titleInput.focus();
+  } else {
+    titleInputError.textContent = "";
+  }
+  if (!bodyInput.validity.valid) {
+    bodyInputError.textContent = "Please enter a message";
+    bodyInput.focus();
+  } else {
+    bodyInputError.textContent = "";
+  }
+  if (blogForm.checkValidity()) {
+    alert("Form Submitted Successfully");
+    createBlogPost();
+    window.location.reload()
+  }
 });
 
-    updateBtn.addEventListener("click", (event)=>{
-    if(event.target.classList.contains('updateBtn')){
-    }
-    if (modalForm.checkValidity()){
-        updateBlogPost()
-        alert("Post updated!")
-        modalUpdate.style.display = 'none'
-        formHalf.style.filter = 'none'
-        postDisplay.style.filter = 'none'
+let recall;
+try {
+  recall = JSON.parse(localStorage.getItem("savedPosts"));
+} catch (error) {
+  console.error("Error parsing items from localStorage", e);
+  recall = null;
 }
-})
-
-
-const recollection = ()=>{
-
-    for(let i=0; i <= recall.length; i++){
-    const newBlogPost = document.createElement("div")
-    newBlogPost.setAttribute("id", recall[i].id)
-    newBlogPost.innerHTML = `${recall[i].title}:<br>${recall[i].body}<br><button class="remove-from-list">Delete</button> <button class="edit">Edit</button>`
-    postDisplay.appendChild(newBlogPost)
-    }
+if (recall == null) {
+  recall = [];
 }
 
-window.addEventListener("load", (event)=>{
-    recollection()
-    clearBtn.style.display='block'
-})
+const createBlogPost = () => {
+  const postObject = {
+    id: objectCounter,
+    title: titleInput.value,
+    body: bodyInput.value,
+  };
 
-clearBtn.addEventListener("click", (event)=>{
-    localStorage.clear()
-    window.location.reload()
-})
+  console.log(postObject);
+  recall.push(postObject);
+  console.log("initial recall: ", recall)
+  
+  localStorage.setItem("savedPosts", JSON.stringify(recall));
+  const newBlogPost = document.createElement("div");
+  newBlogPost.setAttribute("id", postObject.id);
+  const blogPostIndex = recall.findIndex(
+    (postObject) => postObject.id == newBlogPost.id
+  );
+  newBlogPost.innerHTML = `${recall[blogPostIndex].title}:<br>${recall[blogPostIndex].body}<br><button class="remove-from-list">Delete</button> <button class="edit">Edit</button>`;
+  postDisplay.appendChild(newBlogPost);
+  bodyInput.value = "";
+  titleInput.value = "";
+  console.log("postObjID:", postObject.id, "newBlogID: ", newBlogPost.id);
+    incObjectCounter()
+};
+
+const updateBlogPost = () => {
+  const postObject = {
+    id: objectCounter,
+    title: titleModal.value,
+    body: bodyModal.value,
+  };
+  recall.push(postObject);
+    incObjectCounter()
+  localStorage.setItem("savedPosts", JSON.stringify(recall));
+  window.location.reload()
+};
+
+postDisplay.addEventListener("click", event => {
+  if (event.target.classList.contains("remove-from-list")) {
+    const postId = event.target.closest("div").id;
+    console.log(postId);
+    const blogPost = document.getElementById(`${postId}`);
+    console.log(blogPost);
+    const blogPostIndex = recall.findIndex(
+        (postObject) => postObject.id == postId
+    );
+    console.log(blogPostIndex);
+    console.log("deleting:", blogPostIndex, recall)
+    postDisplay.removeChild(blogPost);
+    recall.splice(blogPostIndex, 1);
+    localStorage.setItem("savedPosts", JSON.stringify(recall));
+    console.log("updated recall:", blogPostIndex, recall)
+
+  } else if (event.target.classList.contains("edit")) {
+    const postId = event.target.closest("div").id;
+    const blogPost = document.getElementById(`${postId}`);
+    const blogPostIndex = recall.findIndex(
+      (postObject) => postObject.id == postId
+    );
+    blurBackground()
+    titleModal.value = recall[blogPostIndex].title;
+    bodyModal.value = recall[blogPostIndex].body;
+    console.log(recall)
+    console.log(recall[blogPostIndex])
+    recall.splice(blogPostIndex, 1);
+
+    postDisplay.removeChild(blogPost);
+  }
+});
+
+const incObjectCounter=()=>{
+    objectCounter++
+    localStorage.setItem('ObjectCounter', objectCounter.toString())
+}
+const blurBackground =()=> {
+    modalUpdate.style.display = "block";
+    formHalf.style.filter = "blur(5px)";
+    postDisplay.style.filter = "blur(5px)";
+}
+const unBlurBackground =()=> {
+    modalUpdate.style.display = "none";
+    formHalf.style.filter = "none";
+    postDisplay.style.filter = "none";
+}
+updateBtn.addEventListener("click", event => {
+    console.log(titleModal.value, bodyModal.value)
+    console.log('in updateBtn: ', recall)
+  if (modalForm.checkValidity()) {
+    event.preventDefault();
+    updateBlogPost();
+    alert("Post updated!");
+    unBlurBackground()
+  }
+});
+
+const recollection = () => {
+  for (let i = 0; i <= recall.length; i++) {
+    const newBlogPost = document.createElement("div");
+    newBlogPost.setAttribute("id", recall[i].id);
+    newBlogPost.innerHTML = `${recall[i].title}:<br>${recall[i].body}<br><button class="remove-from-list">Delete</button> <button class="edit">Edit</button>`;
+    postDisplay.appendChild(newBlogPost);
+  }
+};
+
+window.addEventListener("load", (event) => {
+  recollection();
+  clearBtn.style.display = "block";
+
+});
+
+clearBtn.addEventListener("click", (event) => {
+  localStorage.clear();
+  window.location.reload();
+});
